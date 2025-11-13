c14bd8de031e4dd85b4967ab8d9f202e300435fd
HarrrrY145 <harry.burton1@education.nsw.gov.au>
2025-11-13 15:04:10 +1100

    Adding waitress for deploying

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 import os
+from waitress import serve
+
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -10,7 +12,8 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8000)
+    
 @app.route('/login_validation')
 def login_valdiation():
     email = request.form.get('email')
